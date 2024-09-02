bb18ab2a4acf1279c3a92e093c9c0b57ca024088
Admin <timardmitro@gmail.com>
2024-09-03 00:26:35 +0300

    nitial committ

--- just/settings.py
+++ just/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -215,4 +215,4 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # Використову
 
 LOGOUT_REDIRECT_URL = '/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
--- staticfiles/main/js/select.728e8d6180ab.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".select").forEach((el) => {
-  el.addEventListener("click", function (e) {
-    console.log(e);
-    if (e.target && e.target.matches(".option")) {
-      this.querySelector(".shown").innerHTML = e.target.innerHTML;
-    }
-    this.classList.toggle("collapsed");
-  });
-});
